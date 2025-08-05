efad22fe966b42fcc00fdae8f0caf0283aeea4c0
Vipin Sharma <vipins3101@gmail.com>
2025-08-05 14:47:37 +0530

    Create-package-json

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -27,11 +27,29 @@ app.get('/api/keys/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
 
+console.log('Mounting /api/seed');
 app.use('/api/seed', seedRouter);
+
+console.log('Mounting /api/products');
 app.use('/api/products', productRouter);
+
+console.log('Mounting /api/users');
 app.use('/api/users', userRouter);
+
+console.log('Mounting /api/orders');
 app.use('/api/orders', orderRouter);
 
+// app.use('/api/seed', seedRouter);
+// app.use('/api/products', productRouter);
+// app.use('/api/users', userRouter);
+// app.use('/api/orders', orderRouter);
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get(/(.*)/, (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
